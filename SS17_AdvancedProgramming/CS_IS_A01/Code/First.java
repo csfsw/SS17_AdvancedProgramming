6d69574ffb220a879db2eb469637ad7223de6242
Khayalbacha <khayalbacha12@gmail.com>
2017-03-07 21:57:17 +0430

    Mistaken added folder deleted~

--- SS17_AdvancedProgramming/CS_IS_A01/Code/First.java
+++
@@ -1,8 +0,0 @@
-
-public class First {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!!!!");
-	}
-
-}
--- SS17_AdvancedProgramming/CS_IS_A01/Code/second.java
+++
@@ -1,5 +0,0 @@
-public class second{
-    public static void main(String args[]){
-        System.out.print("Hello github!!!!!");
-    }
-}
