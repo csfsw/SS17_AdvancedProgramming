f29db573d3ba2f4bd8778bb666783dd37e9c13d1
esmatullahmohabat <esmatullah.mohabat1@gmail.com>
2017-05-17 22:11:26 -0700

    Add files via upload
    These are the requirements of PatientRegistration Project.

---
+++ Patient_Registration_Requirements.java
@@ -0,0 +1,11 @@
+PatientRegistration:
+We Want to Register the Patient in  hospital.In hospital ,there will be many employees include a Receiptionest
+In first the Patient will comming to the Reciptionest and he or she will register himeself with Receiptionest.
+Recieptionest will ask him about his/her name,address,gender ,sickness............
+The Recieption will refer him to the related doctor and he or she will asign the nurse to the sick.
+the doctor will treat the sick if he was capable to bed the doctor  will assgin him a nurse ,room,bed and many more things.
+if the patient wasn't capable to bed the doctor will write a prescription for ill and the ill will be getting out
+from the hospital.
+if we know we have different types of ills some of them are comming many times like the children for wakseen we will
+Register them by id and every time they will be treated by id. 
+Over the patient information the Recieptionest will store the information of employees who are working in the hospital 
